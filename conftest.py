d7704d35479e415baa3fc3afb562752941238373
Victor Pasenchuk <ucsofworks@gmail.com>
2026-06-24 21:58:18 +0400

    remove Contributing section from README; fix UsersConfig.name; add conftest + pytest config

---
+++ conftest.py
@@ -0,0 +1,22 @@
+def pytest_configure(config):
+    from django.conf import settings
+    if not settings.configured:
+        settings.configure(
+            SECRET_KEY="test-secret-key-not-for-production",
+            INSTALLED_APPS=[
+                "django.contrib.contenttypes",
+                "django.contrib.auth",
+                "stapel_core.django.users",
+                "rest_framework",
+                "stapel_workspaces",
+            ],
+            AUTH_USER_MODEL="users.User",
+            DATABASES={
+                "default": {
+                    "ENGINE": "django.db.backends.sqlite3",
+                    "NAME": ":memory:",
+                }
+            },
+            DEFAULT_AUTO_FIELD="django.db.models.BigAutoField",
+            USE_TZ=True,
+        )
